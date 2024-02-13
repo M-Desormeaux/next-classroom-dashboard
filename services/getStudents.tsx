50b14a2f2346c1ea3145d188185d038db67629dd
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-02-12 23:40:28 -0600

    chore/type GetStudents api

--- services/getStudents.tsx
+++ services/getStudents.tsx
@@ -1,13 +1,31 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
+interface Grade {
+  score: number;
+  classLabel: {
+    label: string;
+  };
+  assignmentLabel: {
+    label: string;
+  };
+}
+
+interface Student {
+  studentID: string;
+  name: string;
+  grades: Grade[];
+}
+
+export type GetStudentsSchema = Student[] | null;
+
 export const getStudents = async () => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data: students } = await supabase
+  const { data: students }: { data: GetStudentsSchema } = await supabase
     .from("students")
     .select(
-      "*, grades(score, aLabel:assignments(label), cLabel:classes(label) )"
+      "*, grades(score, assignmentLabel:assignments(label), classLabel:classes(label) )"
     );
 
   return students;
