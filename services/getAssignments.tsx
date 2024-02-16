98658ade415b0a1c2fde65a387a30b0311c4cfe5
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-02-15 18:28:58 -0600

    enhance services to also include filtered data

--- services/getAssignments.tsx
+++ services/getAssignments.tsx
@@ -1,3 +1,4 @@
+import { getScores } from "@/utils/getScores";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
@@ -17,12 +18,42 @@ interface Assignment {
 
 type GetAssignmentsSchema = Assignment[] | null;
 
+export interface FormattedAssignment {
+  assignmentID: string;
+  classID: string;
+  label: string;
+  classLabel: string;
+  grades: number[];
+
+  min: number;
+  max: number;
+  difference: number;
+  avg: number;
+}
+
 export const getAssignments = async () => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data: assignments }: { data: GetAssignmentsSchema } = await supabase
+  const { data }: { data: GetAssignmentsSchema } = await supabase
     .from("assignments")
     .select("*, classLabel:classes(label), grades(score)");
 
-  return assignments;
+  const formatted: FormattedAssignment[] | null =
+    data?.map((assignment) => {
+      const grades = assignment.grades.map((grade) => grade.score);
+
+      const { min, max, difference, avg } = getScores(grades);
+
+      return {
+        ...assignment,
+        grades,
+        classLabel: assignment.classLabel.label,
+        min,
+        max,
+        difference,
+        avg,
+      };
+    }) ?? null;
+
+  return { raw: data, formatted };
 };
