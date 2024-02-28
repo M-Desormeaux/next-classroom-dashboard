afe73b8d1e2b0c9ab576a2e200cb15df23c2d307
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-02-27 18:35:25 -0600

    chore/add tailwind xs value for desired range

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
-        },
-      },
-    },
+    extend: { screens: { xs: "420px" } },
   },
   plugins: [],
 };
