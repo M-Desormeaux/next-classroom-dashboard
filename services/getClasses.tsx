c07350a979c0ae9e5e21fe47651e6869f9e5ad68
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-03-12 14:31:42 -0500

    improve classes db call

--- services/getClasses.tsx
+++ services/getClasses.tsx
@@ -1,10 +1,28 @@
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
+interface Class {
+  classID: string;
+  label: string;
+  start: string;
+  end: string;
+  teacher: string;
+}
+
 export const getClasses = async () => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  const { data }: { data: any } = await supabase.from("classes").select();
+  const { data }: { data: Class[] | null } = await supabase
+    .from("classes")
+    .select();
 
-  return data;
+  const sortedData = data?.sort((a, b) => {
+    const check =
+      new Date("01/01/2000 " + a.start).getTime() -
+      new Date("01/01/2000 " + b.start).getTime();
+
+    return check;
+  });
+
+  return sortedData;
 };
