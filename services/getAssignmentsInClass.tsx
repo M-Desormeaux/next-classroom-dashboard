4da1d5de7e6debe0bc25a76c92ff85ddfbbf60b5
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-03-12 14:32:13 -0500

    added class assignment db call

---
+++ services/getAssignmentsInClass.tsx
@@ -0,0 +1,60 @@
+import { getScores } from "@/utils/getScores";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+interface Grade {
+  score: number;
+}
+
+interface Assignment {
+  assignmentID: string;
+  classID: string;
+  label: string;
+  classLabel: {
+    label: string;
+  };
+  grades: Grade[];
+}
+
+type GetAssignmentsSchema = Assignment[] | null;
+
+export interface FormattedAssignment {
+  assignmentID: string;
+  classID: string;
+  label: string;
+  classLabel: string;
+  grades: number[];
+
+  min: number;
+  max: number;
+  difference: number;
+  avg: number;
+}
+
+export const getAssignmentsInClass = async (id: string) => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data }: { data: GetAssignmentsSchema } = await supabase
+    .from("assignments")
+    .select("*, classLabel:classes(label), grades(score)")
+    .eq("classID", id);
+
+  const formatted: FormattedAssignment[] | null =
+    data?.map((assignment) => {
+      const grades = assignment.grades.map((grade) => grade.score);
+
+      const { min, max, difference, avg } = getScores(grades);
+
+      return {
+        ...assignment,
+        grades,
+        classLabel: assignment.classLabel.label,
+        min,
+        max,
+        difference,
+        avg,
+      };
+    }) ?? null;
+
+  return formatted;
+};
