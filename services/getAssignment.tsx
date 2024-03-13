6ba2a70f4c5ba28228a9badf40f3fea2d7eee370
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-03-13 14:11:20 -0500

    add assignment db calls

---
+++ services/getAssignment.tsx
@@ -0,0 +1,84 @@
+import { getScores } from "@/utils/getScores";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
+interface Grade {
+  score: number;
+  classID: string;
+  name: string;
+  teacher: string;
+  classLabel: string;
+  gradeID: string;
+  studentID: string;
+  assignmentID: string;
+}
+
+interface Assignment {
+  assignmentID: string;
+  classID: string;
+  label: string;
+  classLabel: {
+    label: string;
+  };
+  grades: {
+    score: number;
+    classID: string;
+    name: { name: string };
+    classLabel: { label: string; teacher: string };
+    gradeID: string;
+    studentID: string;
+    assignmentID: string;
+  }[];
+}
+
+type GetAssignmentsSchema = Assignment[] | null;
+
+export interface FormattedAssignment {
+  assignmentID: string;
+  classID: string;
+  teacher: string;
+  label: string;
+  classLabel: string;
+  grades: Grade[];
+
+  min: number;
+  max: number;
+  difference: number;
+  avg: number;
+}
+
+export const getAssignment = async (id: string) => {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data }: { data: GetAssignmentsSchema } = await supabase
+    .from("assignments")
+    .select(
+      "*, classLabel:classes(*), grades(*, classLabel:classes(label), name:students(name))",
+    )
+    .eq("assignmentID", id);
+
+  const formatted: FormattedAssignment[] | null =
+    data?.map((assignment) => {
+      const grades = assignment.grades.map((grade) => grade.score);
+      const formattedGrades = assignment.grades.map((grade) => ({
+        ...grade,
+        name: grade.name.name,
+        classLabel: grade.classLabel.label,
+      }));
+
+      const { min, max, difference, avg } = getScores(grades);
+
+      return {
+        ...assignment,
+        grades: formattedGrades,
+        teacher: assignment.classLabel?.teacher,
+        classLabel: assignment.classLabel.label,
+        min,
+        max,
+        difference,
+        avg,
+      };
+    }) ?? null;
+
+  return formatted;
+};
