e939d02ddee0e5261a862a11ef2355408f18d740
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-04-10 10:57:55 -0500

    fix error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -4,29 +4,19 @@ import { sans, serif } from "../fonts";
 
 // Error components must use client
 
-export default function GlobalError({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
+export default function GlobalError() {
   return (
-    <html lang="en" className={sans.className}>
-      <body className="flex min-h-svh flex-col items-center">
-        <main className="flex min-h-lvh items-center px-10">
-          <div className="rounded border-2 border-red-600 bg-red-50 px-6 py-4">
-            <h1 className={`text-xl font-bold ${serif.className}`}>
-              Woah, something Critical happened.
-            </h1>
-            <p>
-              I apologize for the inconvenience and I am, likely, fixing this as
-              we speak.
-            </p>
-            {/* <button onClick={() => reset()}>Try again</button> */}
-          </div>
-        </main>
-      </body>
-    </html>
+    <main className="flex min-h-lvh items-center px-10">
+      <div className="rounded border-2 border-red-600 bg-red-50 px-6 py-4">
+        <h1 className={`text-xl font-bold ${serif.className}`}>
+          Woah, something Critical happened.
+        </h1>
+        <p>
+          I apologize for the inconvenience and I am, likely, fixing this as we
+          speak.
+        </p>
+        {/* <button onClick={() => reset()}>Try again</button> */}
+      </div>
+    </main>
   );
 }
