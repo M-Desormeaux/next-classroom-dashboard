cf5e402154b7c4e079abc6494be9063b828f7b0f
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-04-10 11:10:59 -0500

    error clean up

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,18 +1,12 @@
 import { Section } from "@/components/Section";
 import { serif } from "../fonts";
 
-export default function GlobalError() {
+export default function ErrorPage() {
   return (
     <Section>
-      <div className="rounded border-2 border-red-600 bg-red-50 px-6 py-4">
-        <h1 className={`text-xl font-bold ${serif.className}`}>
-          Woah, something Critical happened.
-        </h1>
-        <p>
-          I apologize for the inconvenience and I am, likely, fixing this as we
-          speak.
-        </p>
-      </div>
+      <h1 className={`text-xl font-bold ${serif.className}`}>
+        Something went wrong, please try again later.
+      </h1>
     </Section>
   );
 }
